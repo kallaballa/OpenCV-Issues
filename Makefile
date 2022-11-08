6f9a2c2d99c27977159a70c45e09821de189cbff
kallaballa <amir@viel-zu.org>
2022-11-08 23:46:53 +0100

    init

---
+++ Makefile
@@ -0,0 +1,51 @@
+CXX      := g++
+CXXFLAGS := -std=c++20 -pthread -fno-strict-aliasing -pedantic -Wall -march=native -flto
+LDFLAGS  := -L/opt/local/lib -flto
+LIBS     := -lnanovg
+.PHONY: all release debian-release info debug asan clean debian-clean distclean 
+DESTDIR := /
+PREFIX := /usr/local
+
+all: release
+
+release: CXXFLAGS += -g0 -O3
+release: dirs
+
+info: CXXFLAGS += -g3 -O0
+info: dirs
+
+debug: CXXFLAGS += -g3 -O0 -rdynamic
+debug: dirs
+
+profile: CXXFLAGS += -g3 -O1
+profile: dirs
+
+unsafe: CXXFLAGS += -g0 -Ofast -DNDEBUG -ffast-math -ftree-vectorizer-verbose=1 -funroll-loops -ftree-vectorize -fno-signed-zeros -fno-trapping-math -frename-registers
+unsafe: dirs
+
+asan: CXXFLAGS += -g3 -O0 -fno-omit-frame-pointer -fsanitize=address
+asan: LDFLAGS += -fsanitize=address
+asan: LIBS+= -lbfd -ldw
+asan: dirs
+
+clean: dirs
+
+export LDFLAGS
+export CXXFLAGS
+export LIBS
+
+dirs:
+	${MAKE} -C src/v4l2-vaapi/ ${MAKEFLAGS} CXX=${CXX} ${MAKECMDGOALS}
+	
+debian-release:
+	${MAKE} -C src/v4l2-vaapi/ ${MAKEFLAGS} CXX=${CXX} release
+
+debian-clean:
+	${MAKE} -C src/v4l2-vaapi/ ${MAKEFLAGS} CXX=${CXX} clean
+	
+install: ${TARGET}
+	true
+
+distclean:
+	true
+
---
+++ src/v4l2-vaapi/v4l2-vaapi.cpp
@@ -0,0 +1,71 @@
+#define CL_TARGET_OPENCL_VERSION 200
+
+//WIDTH and HEIGHT have to be specified before including subsystems.hpp
+constexpr long unsigned int WIDTH = 1920;
+constexpr long unsigned int HEIGHT = 1080;
+constexpr const int VA_HW_DEVICE_INDEX = 0;
+
+#include "../common/subsystems.hpp"
+#include <string>
+
+using std::cerr;
+using std::endl;
+using std::string;
+
+
+int main(int argc, char **argv) {
+    using namespace kb;
+
+    if(argc != 2) {
+        cerr << "Usage: video-demo <video-file>" << endl;
+        exit(1);
+    }
+    //Initialize OpenCL Context for VAAPI
+    va::init();
+
+    //Initialize MJPEG HW decoding using VAAPI
+    cv::VideoCapture capture(argv[1], cv::CAP_FFMPEG, {
+            cv::CAP_PROP_HW_DEVICE, VA_HW_DEVICE_INDEX,
+            cv::CAP_PROP_HW_ACCELERATION, cv::VIDEO_ACCELERATION_VAAPI,
+            cv::CAP_PROP_HW_ACCELERATION_USE_OPENCL, 1
+    });
+    // check if we succeeded
+    if (!capture.isOpened()) {
+        cerr << "ERROR! Unable to open camera" << endl;
+        return -1;
+    }
+
+    double fps = capture.get(cv::CAP_PROP_FPS);
+    cerr << "FPS from stream: " << fps << endl;
+    cerr << "VA Version: " << va::get_info() << endl;
+
+    uint64_t cnt = 1;
+    int64 start = cv::getTickCount();
+    double tickFreq = cv::getTickFrequency();
+    double lastFps = fps;
+
+    cv::UMat videoFrame;
+    while (true) {
+        //Bind the OpenCL context for VAAPI
+        va::bind();
+        //Decode a frame on the GPU using VAAPI
+        capture >> videoFrame;
+        if (videoFrame.empty()) {
+            cerr << "End of stream. Exiting" << endl;
+            break;
+        }
+
+        //Measure FPS
+        if (cnt % uint64(ceil(lastFps)) == 0) {
+            int64 tick = cv::getTickCount();
+            lastFps = tickFreq / ((tick - start + 1) / cnt);
+            cerr << "FPS : " << lastFps << '\r';
+            start = tick;
+            cnt = 1;
+        }
+
+        ++cnt;
+    }
+
+    return 0;
+}
