7f6e82f169322397d3a931ff44721e5436d2ddd5
kallaballa <amir@viel-zu.org>
2022-11-26 23:31:00 +0100

    simplified demos

--- src/vaapi-decode-encode/vaapi-decode-encode.cpp
+++ src/vaapi-decode-encode/vaapi-decode-encode.cpp
@@ -3,22 +3,20 @@
 constexpr const int VA_HW_DEVICE_INDEX = 0;
 constexpr const char* OUTPUT_FILENAME = "vaapi-decode-encode.mkv";
 
-#include "../common/subsystems.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
 #include <string>
+#include "../common/subsystems.hpp"
 
 using std::cerr;
 using std::endl;
 using std::string;
 
 int main(int argc, char **argv) {
-    using namespace kb;
-
     if(argc != 2) {
         cerr << "Usage: vaapi-decode-encode <video-file/device-file>" << endl;
         exit(1);
     }
-    //Initialize OpenCL Context for VAAPI
-    va::init();
 
     //Initialize HW decoding using VAAPI
     cv::VideoCapture capture(argv[1], cv::CAP_FFMPEG, {
@@ -43,7 +41,6 @@ int main(int argc, char **argv) {
     });
 
     cerr << "FPS from stream: " << fps << endl;
-    cerr << "VA Version: " << va::get_info() << endl;
 
     uint64_t cnt = 1;
     int64 start = cv::getTickCount();
